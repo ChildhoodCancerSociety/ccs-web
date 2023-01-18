af734f7499ce140acaca8a0ff8a1fe34ce530864
Ibrahim Saberi <georgeipsum@gmail.com>
2023-01-18 18:30:24 -0500

    update tailwind config to include css files in pages and components directories (not sure if this is needed since they are imported into jsx)

--- ccs-next/tailwind.config.js
+++ ccs-next/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx,css}",
+    "./components/**/*.{js,ts,jsx,tsx,css}",
     "./styles/**/*.css"
   ],
   theme: {
