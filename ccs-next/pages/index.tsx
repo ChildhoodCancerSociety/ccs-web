f87d047eaa0c60497019c69c494c368075072239
Ibrahim Saberi <georgeipsum@gmail.com>
2023-01-19 13:45:19 -0500

    prettier config test

--- ccs-next/pages/index.tsx
+++ ccs-next/pages/index.tsx
@@ -1,7 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import { Inter } from "@next/font/google";
+
+import Head from "next/head";
+import Image from "next/image";
+
+import styles from "../styles/Home.module.css";
 
 // const inter = Inter({ subsets: ['latin'] })
 
@@ -21,41 +23,16 @@ export default function Home() {
             <code className={styles.code}>pages/index.tsx</code>
           </p>
           <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
+            <a href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app" target="_blank" rel="noopener noreferrer">
+              By <Image src="/vercel.svg" alt="Vercel Logo" className={styles.vercelLogo} width={100} height={24} priority />
             </a>
           </div>
         </div>
 
         <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
+          <Image className={styles.logo} src="/next.svg" alt="Next.js Logo" width={180} height={37} priority />
           <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
+            <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
           </div>
         </div>
 
@@ -64,60 +41,47 @@ export default function Home() {
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
             target="_blank"
-            rel="noopener noreferrer"
-          >
+            rel="noopener noreferrer">
             <h2 className={""}>
               Docs <span>-&gt;</span>
             </h2>
-            <p className={""}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
+            <p className={""}>Find in-depth information about Next.js features and&nbsp;API.</p>
           </a>
 
           <a
             href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
             target="_blank"
-            rel="noopener noreferrer"
-          >
+            rel="noopener noreferrer">
             <h2 className={""}>
               Learn <span>-&gt;</span>
             </h2>
-            <p className={""}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
+            <p className={""}>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
           </a>
 
           <a
             href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
             target="_blank"
-            rel="noopener noreferrer"
-          >
+            rel="noopener noreferrer">
             <h2 className={""}>
               Templates <span>-&gt;</span>
             </h2>
-            <p className={""}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
+            <p className={""}>Discover and deploy boilerplate example Next.js&nbsp;projects.</p>
           </a>
 
           <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
             target="_blank"
-            rel="noopener noreferrer"
-          >
+            rel="noopener noreferrer">
             <h2 className={""}>
               Deploy <span>-&gt;</span>
             </h2>
-            <p className={""}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
+            <p className={""}>Instantly deploy your Next.js site to a shareable URL with&nbsp;Vercel.</p>
           </a>
         </div>
       </main>
     </>
-  )
+  );
 }
