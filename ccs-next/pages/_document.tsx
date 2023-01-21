ac9cda5e99519017dfa4fc8b5036eebc9ac0e8de
Ibrahim Saberi <georgeipsum@gmail.com>
2023-01-21 11:06:07 -0500

    import react

--- ccs-next/pages/_document.tsx
+++ ccs-next/pages/_document.tsx
@@ -1,10 +1,11 @@
+import React from "react";
 import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" style={{ height: "100%" }}>
       <Head />
-      <body>
+      <body className="h-[100%]">
         <Main />
         <NextScript />
       </body>
