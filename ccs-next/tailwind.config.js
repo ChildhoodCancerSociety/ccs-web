d084e9967f92a52f8dadbdd574f964e78941a780
Ibrahim Saberi <georgeipsum@gmail.com>
2023-01-21 17:21:06 -0500

    add test colors and plugins

--- ccs-next/tailwind.config.js
+++ ccs-next/tailwind.config.js
@@ -7,6 +7,24 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      ccs: {
+        "50": "#FAEEFF",
+        "100": "#F4DBFF",
+        "200": "#B991CC",
+        "300": "#84519D",
+        "400": "#663081",
+        "500": "#4F186B",
+        "600": "#40085C",
+        "700": "#34034D",
+        "800": "#2A043F",
+        "900": "#230533"
+      }
+    }
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/line-clamp")
+  ],
 };
