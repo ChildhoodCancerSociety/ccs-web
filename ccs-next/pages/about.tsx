822e1ca114cddd3600858df9cc660b53f5493a9f
Ibrahim Saberi <georgeipsum@gmail.com>
2023-01-21 11:06:28 -0500

    about page

--- ccs-next/pages/about.tsx
+++ ccs-next/pages/about.tsx
@@ -1,5 +1,39 @@
-const AboutPage: React.FC = ({}) => {
-  return null;
+import React from "react";
+import Head from "next/head";
+import TabbedPage from "../structure/tabbed-page";
+
+const AboutPageOld: React.FC = function AboutPageOld() {
+  return (
+    <>
+      <Head>
+        <title />
+      </Head>
+      <main>
+        <div>
+          test 1
+        </div>
+      </main>
+    </>
+  );
+};
+
+const AboutPageNew: React.FC = function AboutPageNew() {
+  return (
+    <>
+      <Head>
+        <title />
+      </Head>
+      <main>
+        <div>
+          test 2
+        </div>
+      </main>
+    </>
+  );
+};
+
+const AboutPage: React.FC = function AboutPage() {
+  return (<TabbedPage components={[{ name: "Old", Component: AboutPageOld }, { name: "New", Component: AboutPageNew }]} />);
 };
 
 export default AboutPage;
