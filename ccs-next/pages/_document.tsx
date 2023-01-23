749a52be5fcbf5aaf5c2d245fabd500a22a8fdb8
Ibrahim Saberi <georgeipsum@gmail.com>
2023-01-22 20:56:54 -0500

    add header and footer to document structure

--- ccs-next/pages/_document.tsx
+++ ccs-next/pages/_document.tsx
@@ -1,5 +1,7 @@
 import { Head, Html, Main, NextScript } from "next/document";
 
+import { Footer, Header } from "ccs@structure";
+
 export default function Document() {
   return (
     <Html lang="en">
@@ -9,7 +11,9 @@ export default function Document() {
         <link href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@100;300;400;700&family=Signika:wght@300;500&display=swap" rel="stylesheet" />
       </Head>
       <body>
+        <Header />
         <Main />
+        <Footer />
         <NextScript />
       </body>
     </Html>
---
+++ ccs-next/structure/header.tsx
@@ -0,0 +1,9 @@
+const Header: React.FC = function FC() {
+  return (
+    <header className="sticky top-0 h-10 bg-primary-200">
+      <div />
+    </header>
+  );
+};
+
+export default Header;
