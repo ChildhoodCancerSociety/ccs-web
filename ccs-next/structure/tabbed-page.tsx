e268c365d879f48088b72edf0e37ab152e7289e6
Ibrahim Saberi <georgeipsum@gmail.com>
2023-01-22 20:58:45 -0500

    add null component to nullify debug component in production, rename util->utils

--- ccs-next/structure/tabbed-page.tsx
+++ ccs-next/structure/tabbed-page.tsx
@@ -1,11 +1,9 @@
 import { Component, FC, useState } from "react";
 import { Tab } from "@headlessui/react";
+import { classNames } from "ccs@util/style";
+import { NullComponent } from "ccs@util/helpers";
 
 const ENV = process.env.NODE_ENV;
-
-function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(" ");
-}
 
 interface TabbedPageProps {
   components: {
@@ -14,42 +12,41 @@ interface TabbedPageProps {
   }[];
 }
 
-const TabbedPage: React.FC<TabbedPageProps> = function TabbedPage({ components }: TabbedPageProps) {
-  return (
-    <div className="w-full h-full">
-      <Tab.Group>
-        <Tab.List className="absolute flex space-x-1 rounded-xl bg-blue-900/20 p-1 right-2 top-2">
-          {components.map((category) => (
-            <Tab
-              key={category.name}
-              className={({ selected }) => classNames(
-                "w-full rounded-lg py-2.5 min-w-[50px] px-3.5 text-sm font-medium leading-5 text-blue-700",
-                "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2",
-                selected
-                  ? "bg-white shadow"
-                  : "text-blue-400 hover:bg-white/[0.12] hover:text-white",
-              )}
-            >
-              { category.name }
-            </Tab>
-          ))}
-        </Tab.List>
-        <Tab.Panels className="w-full h-full">
-          {components.map((posts, idx) => (
-            <Tab.Panel
-              key={posts.name}
-              className={classNames(
-                "rounded-xl bg-white p-3",
-                "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2",
-              )}
-            >
-              <posts.Component />
-            </Tab.Panel>
-          ))}
-        </Tab.Panels>
-      </Tab.Group>
-    </div>
-  );
-};
+const TabbedPage: React.FC<TabbedPageProps> = process.env.NODE_ENV === "production"
+  ? NullComponent
+  : function TabbedPage({ components }: TabbedPageProps) {
+    return (
+      <div className="w-full h-full">
+        <Tab.Group>
+          <Tab.List className="absolute flex space-x-1 rounded-xl bg-blue-900/20 p-1 right-2 top-2">
+            {components.map((category) => (
+              <Tab
+                key={category.name}
+                className={({ selected }) => classNames(
+                  "w-full rounded-lg py-2.5 min-w-[50px] px-3.5 text-sm font-medium leading-5 text-blue-700",
+                  "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2",
+                  selected
+                    ? "bg-white shadow"
+                    : "text-blue-400 hover:bg-white/[0.12] hover:text-white",
+                )}
+              >
+                { category.name }
+              </Tab>
+            ))}
+          </Tab.List>
+          <Tab.Panels className="w-full h-full">
+            {components.map((posts, idx) => (
+              <Tab.Panel
+                key={posts.name}
+                className="h-full w-full"
+              >
+                <posts.Component />
+              </Tab.Panel>
+            ))}
+          </Tab.Panels>
+        </Tab.Group>
+      </div>
+    );
+  };
 
 export default TabbedPage;
