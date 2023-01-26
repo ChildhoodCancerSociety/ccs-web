88594ae6ac65654e4265426f95f963c541c9ff64
Ibrahim Saberi <georgeipsum@gmail.com>
2023-01-25 21:11:53 -0500

    change base document title

--- ccs-next/pages/_document.tsx
+++ ccs-next/pages/_document.tsx
@@ -8,6 +8,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@100;300;400;700&family=Signika:wght@300;500&display=swap" rel="stylesheet" />
+        <title>Childhood Cancer Society</title>
       </Head>
       <body>
         <Main />
